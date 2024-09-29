6e710a9a9645c303931c4f4a4ae88e51c9c9d1fb
Kamil152 <kama.yussipov@gmail.com>
2024-09-29 11:39:38 +0300

    Add files via upload

---
+++ Переменные.py
@@ -0,0 +1,6 @@
+print('hyppe')
+homework=12
+hour=1.5
+Kurs='Python'
+time=hour/homework
+print('Курс:',Kurs,',всего задач:',homework,',затрачено часов:',hour,',cреднее время выполнения:',time,' часа')
